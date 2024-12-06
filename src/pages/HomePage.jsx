085cc9a3e333e7466c253df283807a27e6b6c00e
netel-pleb <cs.eros111@gmail.com>
2024-12-06 15:13:22 -0300

    fix: compponents url

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Container } from '@mui/material';
-import WelcomeBanner from '../components/home/WelcomeBanner';
-import FeaturedMetrics from '../components/home/FeaturedMetrics';
-import TopPoolsSnapshot from '../components/home/TopPoolsSnapshot';
-import RecentEvents from '../components/home/RecentEvents';
-import KeyFeatures from '../components/home/KeyFeatures';
-import CallToAction from '../components/home/CallToAction';
+import WelcomeBanner from '../components/Home/WelcomeBanner';
+import FeaturedMetrics from '../components/Home/FeaturedMetrics';
+import TopPoolsSnapshot from '../components/Home/TopPoolsSnapshot';
+import RecentEvents from '../components/Home/RecentEvents';
+import KeyFeatures from '../components/Home/KeyFeatures';
+import CallToAction from '../components/Home/CallToAction';
 
 const HomePage = () => {
   return (
